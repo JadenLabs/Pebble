d3ed8e87c65addaa74f4bb99273a9b37630cb3e7
Jaden <100730821+JadenLabs@users.noreply.github.com>
2024-05-05 23:33:46 -0600

    feat(cog): add dm channel check to message event

--- src/bot/cogs/message.py
+++ src/bot/cogs/message.py
@@ -1,4 +1,4 @@
-from discord import Message, Embed
+from discord import Message, Embed, DMChannel
 from discord.ext.commands import Cog, CheckFailure
 from src.bot.bot import Bot
 from src.base.config import config
@@ -17,19 +17,12 @@ class Message(Cog):
     async def on_message(self, message: Message):
         if message.author.bot:
             return
+        if isinstance(message.channel, DMChannel):
+            return
 
         logger.debug(f"Message create: ${message.guild.id} @{message.author.id}")
 
         user_doc, user_is_new = user.find_or_create(message)
-
-        if user_is_new:
-            disclaimer_embed = Embed(
-                color=config.colors["primary"],
-                description=f"Hello {message.author.mention}, sorry to slide into your DMs like this. To respect your privacy, I'd like to let you know the following:\n>>> {config.messages['disclaimer']}",
-            ).set_footer(
-                text="There shouldn't be another dm from me in the future, so sleep tight."
-            )
-            await message.author.send(embed=disclaimer_embed)
 
         await user.handle_msg_xp_event(message)
 
